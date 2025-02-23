19470ae7be8abbecd9ae10f985e094b5944895ab
Abduallah-Shaklaoon <abduallah.shaklaoon@edu.sait.ca>
2025-02-22 22:15:41 -0700

    removed api path from apiurls

--- WhatTheTruckApp-Web/backend/api/urls.py
+++ WhatTheTruckApp-Web/backend/api/urls.py
@@ -1,34 +1,35 @@
-from django.urls import path
+from django.urls import path, include
 from .views import *
+
 
 urlpatterns = [
 
     # User URLs
-    path('api/user/data/', getData, name='get_user_data'),
-    path('api/user/<int:pk>/', getUser, name='get_user'),
-    path('api/user/add/', addUser, name='add_user'),
-    path('api/user/update/<int:pk>/', updateUser, name='update_user'),
-    path('api/user/delete/<int:pk>/', deleteUser, name='delete_user'),
+    path('user/data/', getData, name='get_user_data'),
+    path('user/<int:pk>/', getUser, name='get_user'),
+    path('user/add/', addUser, name='add_user'),
+    path('user/update/<int:pk>/', updateUser, name='update_user'),
+    path('user/delete/<int:pk>/', deleteUser, name='delete_user'),
 
     # Truck URLs
-    path('api/truck/data/', getData, name='get_truck_data'),
-    path('api/truck/<int:pk>/', getTruck, name='get_truck'),
-    path('api/truck/add/', addTruck, name='add_truck'),
-    path('api/truck/update/<int:pk>/', updateTruck, name='update_truck'),
-    path('api/truck/delete/<int:pk>/', deleteTruck, name='delete_truck'),
+    path('truck/data/', getData, name='get_truck_data'),
+    path('truck/<int:pk>/', getTruck, name='get_truck'),
+    path('truck/add/', addTruck, name='add_truck'),
+    path('truck/update/<int:pk>/', updateTruck, name='update_truck'),
+    path('truck/delete/<int:pk>/', deleteTruck, name='delete_truck'),
 
     # Trailer URLs
-    path('api/trailer/data/', getData, name='get_trailer_data'),
-    path('api/trailer/<int:pk>/', getTrailer, name='get_trailer'),
-    path('api/trailer/add/', addTrailer, name='add_trailer'),
-    path('api/trailer/update/<int:pk>/', updateTrailer, name='update_trailer'),
-    path('api/trailer/delete/<int:pk>/', deleteTrailer, name='delete_trailer'),
+    path('trailer/data/', getData, name='get_trailer_data'),
+    path('trailer/<int:pk>/', getTrailer, name='get_trailer'),
+    path('trailer/add/', addTrailer, name='add_trailer'),
+    path('trailer/update/<int:pk>/', updateTrailer, name='update_trailer'),
+    path('trailer/delete/<int:pk>/', deleteTrailer, name='delete_trailer'),
 
     # Log URLs
-    path('api/log/data/', getData, name='get_log_data'),
-    path('api/log/<int:pk>/', getLog, name='get_log'),
-    path('api/log/add/', addLog, name='add_log'),
-    path('api/log/update/<int:pk>/', updateLog, name='update_log'),
-    path('api/log/delete/<int:pk>/', deleteLog, name='delete_log'),
+    path('log/data/', getData, name='get_log_data'),
+    path('log/<int:pk>/', getLog, name='get_log'),
+    path('log/add/', addLog, name='add_log'),
+    path('log/update/<int:pk>/', updateLog, name='update_log'),
+    path('log/delete/<int:pk>/', deleteLog, name='delete_log'),
     
 ]
