3dbe7517773f76f0ffbbbae9f5033d4a3e73fb8a
KrisSenger <kristopher.senger@edu.sait.ca>
2025-02-25 12:21:04 -0700

    Custom user for database so both user types can access JWT tokens

--- WhatTheTruckApp-Web/backend/api/models.py
+++ WhatTheTruckApp-Web/backend/api/models.py
@@ -1,20 +1,48 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
 
 # Create your models here.
 
-class WTT_User(models.Model):
-    userID = models.AutoField(primary_key=True)
+class WTT_UserManager(BaseUserManager):
+    def create_user(self, username, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        return self.create_user(username, email, password, **extra_fields)
+
+class WTT_User(AbstractBaseUser, PermissionsMixin):
+    
+    id = models.AutoField(primary_key=True)
     employeeID = models.CharField(max_length=100)
     status = models.CharField(max_length=1)
     first_name = models.CharField(max_length=40)
     last_name = models.CharField(max_length=40)
-    username = models.CharField(max_length=30)
-    password = models.CharField(max_length=20)
-    address = models.CharField(max_length=50)
+    email = models.EmailField(max_length=100, unique=True)
+    username = models.CharField(max_length=30, unique=True)
+    password = models.CharField(max_length=128)
+    address = models.CharField(max_length=128)
     driver_license = models.CharField(max_length=10)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
+
+    objects = WTT_UserManager()
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
 
     class Meta:
-        db_table = 'WTT_User' 
+        db_table = 'WTT_User'
+
 
 
 class WTT_Truck(models.Model):
--- WhatTheTruckApp-Web/backend/api/serializers.py
+++ WhatTheTruckApp-Web/backend/api/serializers.py
@@ -6,7 +6,20 @@ class UserSerializer(serializers.ModelSerializer):
         model = WTT_User
         fields = '__all__'
         extra_kwargs = {"password": {"write_only": True, "required": True}}
-        
+    
+    def create(self, validated_data):
+        user = WTT_User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password'],    
+            first_name=validated_data.get('first_name', ''),
+            last_name=validated_data.get('last_name', ''),
+            employeeID=validated_data.get('employeeID', ''),
+            status=validated_data.get('status', ''),
+            address=validated_data.get('address', ''),
+            driver_license=validated_data.get('driver_license', '')
+        )
+        return user
 
 class TruckSerializer(serializers.ModelSerializer):
     class Meta:
