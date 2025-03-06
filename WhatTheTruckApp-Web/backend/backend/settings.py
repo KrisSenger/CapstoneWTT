2d41c8fb19833e18727775c299cca311f0053a84
KrisSenger <kristopher.senger@edu.sait.ca>
2025-03-05 20:31:20 -0700

    .

--- WhatTheTruckApp-Web/backend/backend/settings.py
+++ WhatTheTruckApp-Web/backend/backend/settings.py
@@ -152,7 +152,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build", "static")
+STATIC_URL = "/staticfiles/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
