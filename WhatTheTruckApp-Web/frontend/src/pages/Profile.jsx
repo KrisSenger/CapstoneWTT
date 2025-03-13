ed60f529e0693de466a75ebad8723e1963ac491f
Abduallah-Shaklaoon <abduallah.shaklaoon@edu.sait.ca>
2025-03-13 15:49:42 -0600

    Started Profile

--- WhatTheTruckApp-Web/frontend/src/pages/Profile.jsx
+++ WhatTheTruckApp-Web/frontend/src/pages/Profile.jsx
@@ -1,8 +1,21 @@
-
+import React, { useState, useEffect} from 'react';
+import api from '../api';
+import ReturnHome from "../components/ReturnHome";
 
 function Profile() {
+    const [user, setUser] = useState([]);
+    useEffect(() => {
+        getUser();
+    }, []);
+    const getUser = async () => {
+        api.get(`/api/user/data/me/`)
+            .then((res) => res.data)
+            .then((data) => { setUser(data); console.log(data) })
+            .catch((error) => console.error(error));
+    }
     return (
         <div>
+            <ReturnHome />
             <h1>Profile</h1>
         </div>
     );
